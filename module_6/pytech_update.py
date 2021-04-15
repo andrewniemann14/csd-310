f577fd3153160380065a2749f77ba16a2a620e48
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2021-04-15 15:03:43 -0500

    Update pytech_update.py

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -18,6 +18,7 @@ for student in students:
 
 # change 1007's last name
 update = db.students.update_one({"student_id": 1007}, {"$set": {"last_name": "Mapleshield"}})
+print("-- STUDENT DOCUMENT 1007 UPDATED --\n")
 
 # print 1007's record
 print("-- DISPLAYING STUDENT DOCUMENT 1007 --")
@@ -25,3 +26,6 @@ student = db.students.find_one({"student_id": 1007})
 print("Student ID: " + str(student["student_id"]))
 print("First name: " + student["first_name"])
 print("Last name: " + student["last_name"])
+print()
+
+input("\nEnd of program, press Enter to continue...")
