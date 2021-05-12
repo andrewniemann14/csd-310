77b83e9cf510dbf15adf0f9d156e0c28aeee95b4
andrewniemann14 <76855278+andrewniemann14@users.noreply.github.com>
2021-05-12 14:13:04 -0500

    Update whatabook.py

--- module_12/whatabook.py
+++ module_12/whatabook.py
@@ -1,4 +1,4 @@
-# Andrew Niemann, 5/11/21, Module 12
+# Andrew Niemann, 5/12/21, Module 12
 
 import mysql.connector
 from mysql.connector import errorcode
@@ -12,24 +12,35 @@ config = {
 }
 
 def show_menu():
-  print("\n\n\t- Main Menu -\n")
-  print("\t1. View Books")
-  print("\t2. View Store Locations")
-  print("\t3. My Account")
-  print("\t4. Exit program")
-  try:
-    choice = int(input("\nEnter a number to navigate the menu: "))
-    return choice
-  except ValueError:
-    print("Please ensure you are entering an integer.")
+  while True:
+    print("\n\n\t- Main Menu -\n")
+    print("\t1. View Books")
+    print("\t2. View Store Locations")
+    print("\t3. My Account")
+    print("\t4. Exit program")
+    choice = 0
+    try:
+      choice = int(input("\nEnter a number to navigate the menu: "))
+    except ValueError:
+      input("Invalid input. Press 'Enter' to try again...")
+      continue
+    if choice == 1:
+      show_books(cursor) # book list and back
+    elif choice == 2: 
+      show_locations(cursor) # location and back
+    elif choice == 3:
+      validate_user(cursor) # valid user_id directs to account menu, else returns
+    elif choice == 4:
+      break # exits loop/function/program
+    else:
+      input("Invalid input. Press 'Enter' to try again...") # repeats loop/function
 
 def show_books(_cursor):
   _cursor.execute("SELECT * FROM book")
   books = cursor.fetchall()
   print("\nList of Available Books:")
   for book in books:
-    print(f"\t{book[0]}. {book[1]}, by {book[3]}")
-  # see_details = input("\nEnter a book's number to see a brief description,\nor press 'Enter' to return to the Main Menu: ")
+    print(f"\t{book[0]}. {book[1]}, by {book[3]}") # print(book_id. book_name, by author)
   input("\nPress 'Enter' to return to the Main Menu...")
   
 def show_locations(_cursor):
@@ -43,16 +54,25 @@ def show_locations(_cursor):
 def validate_user(_cursor):
   _cursor.execute("SELECT * FROM user")
   user_fetch = _cursor.fetchall() # returns list of tuples
+  # the below technique to check input against SQL query results is also used in show_books_to_add()
+  # inspiration came from https://stackoverflow.com/questions/1553275/how-to-strip-a-list-of-tuple-with-python
   userlist = [] # creates regular list that will be used
   for row in user_fetch:
     userlist.append(int(row[0])) # adds first element of each SQL tuple to new list
-  login = int(input("Please enter your user ID: "))
+  try:
+    login = int(input("Please enter your user ID: "))
+  except:
+    login_invalid = input("Invalid input. Enter '1' to try again, or press 'Enter' to return to the Main Menu: ")
+    if login_invalid == "1":
+      validate_user(_cursor) # restarts user_id prompt
+    else:
+      return None # exits loop/function, returns to main menu
   if login in userlist: # checks user input against list of user_ids
     print(f"\nWelcome back, {user_fetch[login-1][1]}!")
     input("Press 'Enter' to continue...")
     show_account_menu(_cursor, login)
   else:
-    input("User not found. Please try again.\n")
+    print("User not found. Please try again.\n")
     validate_user(_cursor)
 
 def show_account_menu(_cursor, _user_id):
@@ -64,15 +84,16 @@ def show_account_menu(_cursor, _user_id):
     try:
       choice = int(input("\nEnter a number to navigate the menu: "))
     except ValueError:
-      print("Please ensure you are entering an integer.")
-    if choice == 1:
+      input("Invalid input. Press 'Enter' to continue...")
+      continue
+    if choice == 1: # wishlist and back
       show_wishlist(_cursor, _user_id)
-    if choice == 2:
+    elif choice == 2: # books to add. from there, "add" function will be called repeatedly until input directs flow back here
       show_books_to_add(_cursor, _user_id)
-    if choice == 3:
+    elif choice == 3: # exits loop/function, returns to main menu
       break
     else:
-      input("Invalid option entered. Press 'Enter' to continue...")
+      input("Invalid input. Press 'Enter' to continue...")
 
 def show_wishlist(_cursor, _user_id):
   _cursor.execute("SELECT user.user_id, book.book_id, book_name FROM wishlist JOIN user ON user.user_id = wishlist.user_id JOIN book ON book.book_id = wishlist.book_id WHERE wishlist.user_id = {}".format(_user_id))
@@ -87,18 +108,18 @@ def show_books_to_add(_cursor, _user_id):
     _cursor.execute("SELECT book_id, book_name FROM book WHERE book_id NOT IN (SELECT book_id FROM wishlist WHERE user_id = {})".format(_user_id))
     wishlist_books = cursor.fetchall()
     print("\nList of books NOT on your wishlist:")
-    booklist = [] # same trick as validating user_id
+    booklist = [] # same trick as user_id validation, this time we will make sure book is not already in wishlist
     for book in wishlist_books:
       print(f"  {book[0]}. {book[1]}")
-      booklist.append(book[0])
+      booklist.append(book[0]) # add the book_id to the list we created
     book_to_add = input("\nPress 'Enter' to return to the previous menu,\nor enter a book's ID number to add it to your wishlist: ")
     if book_to_add == "":
-      break
-    elif int(book_to_add) not in booklist:
+      break # exits loop/function, returns to account menu
+    elif int(book_to_add) not in booklist: # check input against list we created
       print(booklist)
       input("Invalid book ID entered. Press 'Enter' to continue...")
     elif int(book_to_add) in booklist:
-      add_book_to_wishlist(_cursor, _user_id, book_to_add)
+      add_book_to_wishlist(_cursor, _user_id, book_to_add) # adds book, restarts show_books_to_add()
 
 def add_book_to_wishlist(_cursor, _user_id, _book_id):
   _cursor.execute(f"INSERT INTO wishlist(user_id, book_id) VALUES({_user_id}, {_book_id})")
@@ -108,15 +129,4 @@ def add_book_to_wishlist(_cursor, _user_id, _book_id):
 
 db = mysql.connector.connect(**config)
 cursor = db.cursor()
-while True:
-  choice = show_menu()
-  if choice == 1: # book list and back
-    show_books(cursor)
-  elif choice == 2: # location and back
-    show_locations(cursor)
-  elif choice == 3: # valid user_id directs to account menu
-    validate_user(cursor)
-  elif choice == 4:
-    break
-  else:
-    input("Invalid input: Press 'Enter' to try again.")
+show_menu()
